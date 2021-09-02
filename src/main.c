f6d888abaacd111d863438d83a3806605db2d923
k <1583471689@qq.com>
2021-09-03 00:14:20 +0800

    first commit

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "nm.h"
+
+int main()
+{
+    return 0;
+}
---
+++ src/nm.h
@@ -0,0 +1,25 @@
+#ifndef __nm_h__
+#define  __nm_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//req/rep;
+
+
+//push/pull;
+
+
+//pub/sub;
+
+
+//survey/vote;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
